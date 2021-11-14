d08dd8a3654b3ff396701153154f3d1f300224d6
M Mauludin Rohman <38213112+mauludinr@users.noreply.github.com>
2021-11-15 01:20:47 +0700

    Update flight.py

--- dataflow/flight.py
+++ dataflow/flight.py
@@ -3,30 +3,33 @@ import argparse
 from apache_beam.options.pipeline_options import PipelineOptions
 from sys import argv
 from apache_beam.dataframe import convert
+import os
 
 PROJECT_ID='red-function-330907'
 
 def run(argv=None):
   parser = argparse.ArgumentParser()
-  known_args, pipeline_args = parser.parse_known_args(argv)
-  
-  with beam.Pipeline(options=PipelineOptions()) as pipeline:
 
+  known_args, pipeline_args = parser.parse_known_args(argv)
+
+  with beam.Pipeline(options=PipelineOptions()) as pipeline:
     extract_data = (
         pipeline 
-        | 'Read CSV with beam.dataframe' >> beam.dataframe.io.read_csv('gs://de-week2-bucket/Data/') 
+        | 'Read CSV with beam.dataframe' >> beam.dataframe.io.read_csv('gs://de-week2-bucket/Data/*.csv') 
     )
-    extract_data.fillna('', inplace=True)
+    
+    extract_data.fillna('',inplace=True)
+    
     transform_data = (
         convert.to_pcollection(extract_data)
-        | 'Convert to Pcollection then to dictionaries' >> beam.Map(lambda x: dict(x._asdict()))
+        | 'Convert to dictionaries' >> beam.Map(lambda x: dict(x._asdict()))
     )
     load_data = (
         transform_data
         | "Write data to BigQuery" >> beam.io.WriteToBigQuery(
-           table= '{0}:flight1.flight2_data'.format(PROJECT_ID),
+           table= '{0}:flight1.all_flight'.format(PROJECT_ID),
            schema='SCHEMA_AUTODETECT',
            write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE)
-           
+    )
 if __name__ == '__main__':
     run()
