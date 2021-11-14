e921021c46a372a5650831acdb298968a62a6a5d
M Mauludin Rohman <38213112+mauludinr@users.noreply.github.com>
2021-11-15 01:22:29 +0700

    Create dag_gcs_tobq.py

---
+++ dag/dag_gcs_tobq.py
@@ -0,0 +1,46 @@
+import os
+from airflow import DAG
+from airflow.providers.apache.beam.operators.beam import BeamRunPythonPipelineOperator
+from airflow.providers.google.cloud.operators.dataflow import DataflowConfiguration
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow import configuration
+from airflow.models import Variable
+from datetime import datetime
+
+
+args = {
+ 
+    'owner': 'mauludinr98',
+    'start_date': datetime(2021,10,30),
+    'retries' : 1,
+}
+
+PROJECT_ID = 'red-function-330907'
+PY_FILE = ('gs://de-week2-bucket/project_flight/dataflow/flight.py')
+
+pipeline_options = {'tempLocation': "gs://de-week2-bucket/batch/stag" ,
+                    'stagingLocation': "gs://de-week2-bucket/batch/temp",
+                        }
+
+
+dag = DAG(dag_id = 'dag_gcs_tobq', default_args=args,catchup=False, schedule_interval='@daily')
+ 
+with dag:
+    dataflow_task = BeamRunPythonPipelineOperator(
+        task_id='job_keyword_search_gcs_to_bigquery',
+        runner='DataflowRunner',
+        gcp_conn_id='google_cloud_default',
+        py_file=PY_FILE,
+        py_requirements=['apache-beam[gcp]==2.34.0'],
+        py_system_site_packages=True,
+        py_interpreter='python3',
+        pipeline_options=pipeline_options,
+        dataflow_config=DataflowConfiguration(
+            job_name="job_keyword_search_{{ ds_nodash }}",
+            project_id=PROJECT_ID,
+            location="asia-southeast1",
+            wait_until_finished=True
+        )
+    )
+    dataflow_task
